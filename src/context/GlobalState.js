faaae6a8472bae82ee862a56ebbda7e92bf1ee57
Quinn94 <mayquinn94@gmail.com>
2020-07-21 08:52:00 +0800

    delete btn

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -17,8 +17,15 @@ export const GlobalContext = createContext(initialState);
 //Provider component -provides state and actions
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  function deleteTransaction(id) {
+    dispatch({ type: "DELETE_TRANSACTION", payload: id });
+  }
+
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{ transactions: state.transactions, deleteTransaction }}
+    >
       {children}
     </GlobalContext.Provider>
   );
